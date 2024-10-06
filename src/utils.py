9fa5421b3352ed0cb61f8fdff606f869d256e8f4
laurent <laurent.copey@gmail.com>
2024-10-06 21:14:09 +0200

    allègement de basestatemeta

--- src/utils.py
+++ src/utils.py
@@ -65,8 +65,6 @@ class BaseStateMeta(ABCMeta):
         {'input': <class 'str'>, 'value': <class 'float'>, 'object': <class 'set'>}
         >>> BaseState._default
         {'value': 10, 'object': set()}
-        >>> isinstance(BaseState.__pre_init__, Callable)
-        True
 
         Args:
             name:
@@ -78,42 +76,8 @@ class BaseStateMeta(ABCMeta):
         dct["_default"] = default_kwargs
         dct["_fields"] = _get_fields(annotations, default_kwargs)
         dct["__annotations__"] = annotations
-
-        def _from_kwargs(self, kwargs: dict):
-            for key, value in kwargs.items():
-                if key in annotations or key in default_kwargs:
-                    setattr(self, key, value)
-                else:
-                    raise ValueError(f"Unexpected keyword {key}")
-
-        def _set_default(self, default_kwargs: dict):
-            for key, value in default_kwargs.items():
-                attr = getattr(self, key, None)
-                if not attr:
-                    setattr(self, key, value)
-
-        def define_attributes(self, *args, **kwargs):
-            _check_required_arg(annotations, default_kwargs, kwargs)
-            # for base in bases:
-            #     for key, obj in base.__dict__.items():
-            #         if not _is_private_or_special(key):
-            #             setattr(self, key, obj)
-            if not kwargs and (args and len(args) == 1):
-                raise NotImplemented()
-            elif kwargs and not args:
-                _from_kwargs(self, kwargs)
-                _set_default(self, default_kwargs)
-            elif not kwargs and not args:
-                _set_default(self, default_kwargs)
-            else:
-                raise TypeError(
-                    "Either one dict argument or multiple keyword arguments only"
-                )
-
-        dct["__pre_init__"] = define_attributes
         self_instance = super().__new__(cls, name, bases, dct)
 
-        # self_instance.__init__ = __new_init__
         return self_instance
 
 
@@ -155,10 +119,10 @@ class BaseState(metaclass=BaseStateMeta):
 
     _fields: list
     _default: dict[str, Any]
-    __pre_init__: Callable
+    __annotations__: dict[str, type]
 
     def __init__(self, *args, **kwargs):
-        self.__pre_init__(*args, **kwargs)
+        self.define_attributes(*args, **kwargs)
         self._watch_for_change = False
         self._parent = None
         self._name = None
@@ -171,6 +135,37 @@ class BaseState(metaclass=BaseStateMeta):
                 self.child = attr
 
         self.change = {}
+
+    def _from_kwargs(self, kwargs: dict):
+        for key, value in kwargs.items():
+            if key in self.__annotations__ or key in self._default:
+                setattr(self, key, value)
+            else:
+                raise ValueError(f"Unexpected keyword {key}")
+
+    def _set_default(self):
+        for key, value in self._default.items():
+            attr = getattr(self, key, None)
+            if not attr:
+                setattr(self, key, value)
+
+    def define_attributes(self, *args, **kwargs):
+        _check_required_arg(self.__annotations__, self._default, kwargs)
+        # for base in bases:
+        #     for key, obj in base.__dict__.items():
+        #         if not _is_private_or_special(key):
+        #             setattr(self, key, obj)
+        if not kwargs and (args and len(args) == 1):
+            raise NotImplemented()
+        elif kwargs and not args:
+            self._from_kwargs(kwargs)
+            self._set_default()
+        elif not kwargs and not args:
+            self._set_default()
+        else:
+            raise TypeError(
+                "Either one dict argument or multiple keyword arguments only"
+            )
 
     def __repr__(self):
         attrs = ", ".join(f"{field}={getattr(self, field)!r}" for field in self._fields)
