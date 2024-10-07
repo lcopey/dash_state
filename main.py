a728d8584ba811ec5e3765f9da38eb1b2b0562cc
Laurent Copey <laurent.copey@michelin.com>
2024-10-07 15:20:00 +0200

    ajout d'évenement pour le stockage initial des composants

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ class StateData(BaseState):
     input: Input = Input()
 
 
-# store = ReduxStore(id='store', data={}, model=StateData)
-# input_ = dcc.Input(id='input')
-#
-# app = Dash()
-# app.layout = html.Div([store, input_])
-#
-#
-# @store.update(Input(input_, 'value'))
-# def update_store(value, state: StateData):
-#     return value
-#
-#
-# if __name__ == '__main__':
-#     app.run()
-
 Redux = ReduxStore(id="store", state_factory=StateData)
 input_ = dcc.Input(id="input")
 markdown = dcc.Markdown(id="markdown")
@@ -32,15 +17,17 @@ markdown = dcc.Markdown(id="markdown")
 app = Dash(prevent_initial_callbacks=True)
 app.layout = html.Div([Redux, input_, markdown])
 
+Redux.store_initial(input_.id, "value", default="")
+
 
 @Redux.update(Input(input_, "value"))
 def update_store(value, state: StateData):
     state.input.value = value
-    return state
 
 
-@callback(Output(markdown, "children"), Redux.store.input)
+@callback(Output(markdown, "children"), Redux.store.as_input)
 def update_markdown(state):
+    print("update_markdown")
     state = StateData.from_dict(state)
     return state.input.value
 
--- src/master_store.py
+++ src/master_store.py
@@ -1,7 +1,17 @@
-from dash import html, dcc, callback, ALL, Input, Output, State, callback_context
+from dash import (
+    html,
+    dcc,
+    callback,
+    ALL,
+    Input,
+    Output,
+    State,
+    callback_context,
+    no_update,
+)
 import sys
 from typing import Any
-from dataclasses import dataclass
+from typing import Literal, Hashable
 from .state import BaseState
 
 from dash.exceptions import PreventUpdate
@@ -11,7 +21,7 @@ class StoreError(TypeError): ...
 
 
 FORGOT_STATE_MSG_ERROR = """
-Les fonctions décorés par ReduxStore.udate doivent prendre l'état de l'application en dernier argument.
+Les fonctions décorées par ReduxStore.update doivent prendre l'état de l'application en dernier argument.
 
 store = ReduxStore(...)
 input = dcc.Input(id=...)
@@ -22,13 +32,50 @@ def callback(value, state):
 
 """
 
+NO_RETURN_MSG_ERROR = """
+Les fonctions décorées par ReduxStore.update ne doivent rien retourner et modifie l'état de l'application
+en mutant directement la variable state passé en argument :
 
-@dataclass
+store = ReduxStore(...)
+input = dcc.Input(id=...)
+
+@store.update(Input(input, 'value')
+def callback(value, state):
+    state.input.value = value
+"""
+
+
 class IdComposer:
-    type: str
+    """
+
+    >>> IdComposer(type='store')
+    IdComposer(type='store')
+    >>> IdComposer(type='store').bind(mode='callback')
+    IdComposer(type='store', mode='callback')
+    >>> IdComposer(type='store').idx('value')
+    {'type': 'store', 'idx': 'value'}
+    >>> IdComposer(type='store').idx(ALL)
+    {'type': 'store', 'idx': <ALL>}
+    >>> IdComposer(type='store').bind(mode='callback').idx(ALL)
+    {'type': 'store', 'mode': 'callback', 'idx': <ALL>}
+
+    """
+
+    def __init__(self, **kwargs):
+        assert (
+            "idx" not in kwargs
+        ), "idx is reserved keyword and should not be in kwargs"
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        attrs = ", ".join(f"{key}={value!r}" for key, value in self.kwargs.items())
+        return f"{self.__class__.__name__}({attrs})"
 
     def _idx(self, value: Any):
-        return {"type": self.type, "idx": value}
+        return {**self.kwargs, "idx": value}
+
+    def bind(self, **kwargs):
+        return IdComposer(**self.kwargs, **kwargs)
 
     def idx(self, value: Any):
         return self._idx(value)
@@ -94,8 +141,8 @@ class ReduxStore(html.Div):
     >>> Redux = ReduxStore('store', state_factory=State)
     >>> Redux
     Div([Store(id='store', data={'input': None}, storage_type='session')])
-    >>> Redux._surrogate_store_match
-    IdComposer(type='store_ip')
+    >>> Redux._surrogate_stores_match
+    IdComposer(type='surrogate_store')
 
     """
 
@@ -106,20 +153,19 @@ class ReduxStore(html.Div):
         data: BaseState | None = None,
         **kwargs,
     ):
-        storage_type = kwargs.pop("storage_type", "session")
+        self._storage_type = kwargs.pop("storage_type", "session")
         self._state_factory = state_factory
         data = data or state_factory.default().as_dict()
         self._master_store = dcc.Store(
-            id=id, storage_type=storage_type, data=data, **kwargs
+            id=id, storage_type=self._storage_type, data=data, **kwargs
         )
 
-        self._surrogate_store_match = IdComposer(type=f"{id}_ip")
-        self._surrogate_stores = dict()
+        self._surrogate_stores_match = IdComposer(type=f"surrogate_{id}")
+        self._surrogate_stores: dict[tuple[Hashable, str], dcc.Store] = dict()
 
         class _Proxy:
-            input = Input(self._master_store, "data")
-            output = Output(self._master_store, "data")
-            state = State(self._master_store, "data")
+            as_input = Input(self._master_store, "data")
+            as_state = State(self._master_store, "data")
 
         self.store = _Proxy
 
@@ -127,42 +173,85 @@ class ReduxStore(html.Div):
 
         @callback(
             Output(self._master_store, "data"),
-            Input(self._surrogate_store_match.idx(ALL), "data"),
+            Input(self._surrogate_stores_match.bind(mode="callback").idx(ALL), "data"),
+            self.store.as_state,
             prevent_initial_call=True,
         )
-        def update_store(surrogate_state):
+        def update_master_store(surrogate_state, current_state: dict):
+            print("update_master_store")
             index = trigger_index()
             if index is not None:
-                state = surrogate_state[index]
-                print("Update master store", state)
-                return state
+                merged = self._state_factory.from_dict(current_state).merge(
+                    surrogate_state[index]
+                )
+
+                return merged.as_dict()
             raise PreventUpdate()
 
-    def _surrogate_input_store(self, *inputs: Input | State) -> dcc.Store:
+    def _surrogate_input_store(
+        self,
+        *inputs: Input | State,
+        mode: Literal["callback", "initial", "initial_state"],
+        default: Any | None = None,
+    ) -> dcc.Store:
+        """TODO documenter celle-ci car plein de branchement"""
         idx = _input_hash(*inputs)
         if idx not in self._surrogate_stores:
             # Initialise les valeurs avec la forme des données du master store
+            store_id = self._surrogate_stores_match.bind(mode=mode).idx(idx)
+            if mode == "callback":
+                storage_type = self._storage_type
+                initial_data = self._master_store.data
+            elif mode == "initial":
+                storage_type = self._storage_type
+                initial_data = default
+            elif mode == "initial_state":
+                storage_type = "memory"
+                initial_data = True
+            else:
+                raise ValueError(f"Unknow mode : {mode}")
+
             store = dcc.Store(
-                id=self._surrogate_store_match.idx(idx),
-                data=self._master_store.data,
-                storage_type="memory",
+                id=store_id,
+                data=initial_data,
+                storage_type=storage_type,
             )
             self.children.append(store)
-            self._surrogate_stores[idx] = store
-        return self._surrogate_stores[idx]
+            self._surrogate_stores[(idx, mode)] = store
+        return self._surrogate_stores[(idx, mode)]
 
-    # def initial_update(self, input_: Input, **callback_kwargs):
-    #     def wrapper(func):
-    #         @callback(
-    #             Output(self.master_store, 'data'),
-    #             input_,
-    #             State(self.master_store, 'data')
-    #         )
-    #         def _proxy():
-    #             pass
+    def store_initial(
+        self, component_id, component_property, default: Any | None, **callback_kwargs
+    ):
+        input_ = Input(component_id=component_id, component_property=component_property)
+
+        surrogate_store_value = self._surrogate_input_store(
+            input_, mode="initial", default=default
+        )
+        surrogate_store_state = self._surrogate_input_store(
+            input_, mode="initial_state"
+        )
+
+        prevent_initial_call = callback_kwargs.pop("prevent_initial_call", False)
+
+        @callback(
+            Output(surrogate_store_value, "data"),
+            Output(surrogate_store_state, "data"),
+            Output(component_id, component_property),
+            input_,
+            Input(surrogate_store_value, "data"),
+            State(surrogate_store_state, "data"),
+            prevent_initial_call=prevent_initial_call,
+        )
+        def _proxy(arg, store: Any, state: bool):
+            print("store_initial", arg, store, state)
+            if state:
+                return no_update, False, store
+            else:
+                return arg, False, no_update
 
     def update(self, *inputs: Input | State, **callback_kwargs):
-        surrogate_store = self._surrogate_input_store(*inputs)
+        surrogate_store = self._surrogate_input_store(*inputs, mode="callback")
 
         def wrapper(func):
             prevent_initial_call = callback_kwargs.pop("prevent_initial_call", True)
@@ -175,21 +264,21 @@ class ReduxStore(html.Div):
                 **callback_kwargs,
             )
             def _proxy(*args):
+                print("update surrogate")
                 args = list(args)
                 state = self._state_factory.from_dict(args.pop())
-                print("Update")
-                print("State avant", state)
 
                 try:
-                    result = func(*args, state)
-                    print("State après", state)
-                    if isinstance(result, self._state_factory):
-                        result = result.as_dict()
+                    with state.watch_for_change() as new_state:
+                        result: Any = func(*args, new_state)
+                        if result is not None:
+                            raise StoreError(NO_RETURN_MSG_ERROR)
+
                 except TypeError as e:
                     if "positional argument" in e.args[0]:
                         raise StoreError(FORGOT_STATE_MSG_ERROR)
                     else:
                         raise e
-                return result
+                return state.change
 
         return wrapper
